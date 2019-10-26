638f60508344f3e973a7f416f3ca7a3ef51c7d45
Lukas Traub <traublukas@yahoo.de>
2019-10-26 19:04:27 +0300

    Add Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
+import { ChatComponent } from './component/chat/chat.component';
+import { MatchesComponent } from './component/matches/matches.component';
+import { UserComponent } from './component/user/user.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'matches', component: MatchesComponent},
+  {path: 'chat', component: ChatComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { HomeComponent } from './component/home/home.component';
+import { UserComponent } from './component/user/user.component';
+import { ChatComponent } from './component/chat/chat.component';
+import { MatchesComponent } from './component/matches/matches.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    UserComponent,
+    ChatComponent,
+    MatchesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
